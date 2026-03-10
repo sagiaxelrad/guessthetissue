8e4cbd73079e8ddce8c6085d30a23ca045c1be6e
unknown <sagi.axelrad@gmail.com>
2026-03-10 16:40:59 +0200

    quiz 1.0.0

---
+++ Controllers/QuizController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace gtt.Controllers
+{
+    public class QuizController : Controller
+    {
+        private static List<Question> questions;
+        private static Random rnd = new Random();
+
+        static QuizController()
+        {
+            Parser parser = new Parser("questions.json");
+            questions = parser.Questions;
+        }
+
+        public IActionResult Question(int id = 1)
+        {
+            if (questions.Count == 0)
+                return Content("No questions loaded.");
+
+            // convert human number → zero-based index
+            id = id - 1;
+
+            if (id < 0)
+                id = 0;
+
+            if (id >= questions.Count)
+                id = questions.Count - 1;
+
+            var q = questions[id];
+
+            var wrongAnswers = questions
+                .Where(x => x.category == q.category)
+                .Select(x => x.answer)
+                .Where(a => a != q.answer)
+                .Distinct()
+                .OrderBy(x => rnd.Next())
+                .Take(3)
+                .ToList();
+
+            var choices = new List<string>(wrongAnswers);
+            choices.Add(q.answer);
+
+            choices = choices.OrderBy(x => rnd.Next()).ToList();
+
+            ViewBag.Question = q;
+            ViewBag.Choices = choices;
+            ViewBag.Id = id;
+            ViewBag.Total = questions.Count;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Answer(string selectedAnswer, string correctAnswer, int id)
+        {
+            bool correct = selectedAnswer == correctAnswer;
+
+            ViewBag.Correct = correct;
+            ViewBag.Selected = selectedAnswer;
+            ViewBag.CorrectAnswer = correctAnswer;
+            ViewBag.NextId = id + 2;
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/parser.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+
+namespace gtt.Controllers
+{
+    public class Question
+    {
+        public string question { get; set; }
+        public string image { get; set; }
+        public string category { get; set; }
+        public string answer { get; set; }
+
+        public Question() 
+        { }
+
+        public Question(string question, string image, string category, string answer)
+        {
+            this.question = question;
+            this.image = image;
+            this.category = category;
+            this.answer = answer;
+        }
+    }
+
+    public class Parser
+    {
+        public List<Question> Questions { get; private set; }
+
+        public Parser(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Questions = new List<Question>();
+                return;
+            }
+
+            string jsonString = File.ReadAllText(filePath
+                );
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var root = JsonSerializer.Deserialize<
+                Dictionary<string, List<Question>>
+            >(jsonString, options);
+
+            Questions = root?["questions"] ?? new List<Question>();
+            foreach (var q in Questions)
+            {
+                q.image = "/Images/" + q.image;
+            }
+        }
+    }
+}
